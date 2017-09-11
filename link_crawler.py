d76cac74623b89cbd9eeffeeb45eafb016e150c8
wangkai19 <1009568202@qq.com>
2017-09-11 17:41:52 +0800

    test1

--- link_crawler.py
+++ link_crawler.py
@@ -1,6 +1,9 @@
 # -*- coding:UTF-8 -*-
 import urllib2
+import re
+import urlparse
 
+#use urllib2 download html by a url
 def download(url,user_agent = 'QiaoBa',num_retries = 2):
     print 'Downloading:',url
     headers = {'User_agent':user_agent}
@@ -15,4 +18,25 @@ def download(url,user_agent = 'QiaoBa',num_retries = 2):
                 return download(url,user_agent,num_retries - 1)
     return html
 
-print download('http://example.webscraping.com')
+#Return a list of lists from html
+def get_links(html):
+    webpage_regex = re.compile('<a[^>]+href=["\'](.*?)["\']',re.IGNORECASE)
+    return webpage_regex.findall(html)
+
+def link_crawler(seed_url,link_regex):
+    crawl_queue = [seed_url]
+    #keep track which URL's have seen before
+    seen = set(crawl_queue)
+    while crawl_queue:
+        url = crawl_queue.pop()
+        html = download(url)
+        for link in get_links(html):
+            if re.match(link_regex,link):
+                #be absolute link
+                link = urlparse.urljoin(seed_url,link)
+                #check if have alreadly seen this link
+                if link not in seen:
+                    seen.add(link)
+                    crawl_queue.append(link)
+
+link_crawler('http://example.webscraping.com','/(places/default/view|places/default/index)')
